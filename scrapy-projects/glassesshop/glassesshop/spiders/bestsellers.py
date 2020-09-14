0f89b0e9f98e7e5f8a6b6d0d822c81cac5a586f4
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-14 21:08:17 +0600

    Completed multipage scraping assignment

--- scrapy-projects/glassesshop/glassesshop/spiders/bestsellers.py
+++ scrapy-projects/glassesshop/glassesshop/spiders/bestsellers.py
@@ -4,8 +4,21 @@ import scrapy
 
 class BestsellersSpider(scrapy.Spider):
     name = 'bestsellers'
-    allowed_domains = ['www.glassesshop.com/bestsellers']
-    start_urls = ['http://www.glassesshop.com/bestsellers/']
+    allowed_domains = ['www.glassesshop.com']
+    start_urls = ['https://www.glassesshop.com/bestsellers']
 
     def parse(self, response):
-        pass
+        glasses = response.xpath("//div[@id='product-lists']/div")
+        for glass in glasses:
+            yield {
+                'name': glass.xpath("normalize-space(.//div[@class='p-title']/a/text())").get(),
+                'url': glass.xpath(".//div[@class='product-img-outer']/a/@href").get(),
+                'img_url': glass.xpath(".//img[@class='lazy d-block w-100 product-img-default']/@src").get(),
+                'price': glass.xpath(".//div[@class='p-price']//span/text()").get()
+            }
+
+        next_page = response.xpath(
+            "//ul[@class='pagination']/li[position() = last()]/a/@href").get()
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
