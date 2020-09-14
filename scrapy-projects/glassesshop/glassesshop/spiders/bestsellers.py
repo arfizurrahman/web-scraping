9e2538f33d50861e992548e59d19397e8e6e1502
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-14 20:32:21 +0600

    Glassesop exercise project initial commit

---
+++ scrapy-projects/glassesshop/glassesshop/spiders/bestsellers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BestsellersSpider(scrapy.Spider):
+    name = 'bestsellers'
+    allowed_domains = ['www.glassesshop.com/bestsellers']
+    start_urls = ['http://www.glassesshop.com/bestsellers/']
+
+    def parse(self, response):
+        pass
