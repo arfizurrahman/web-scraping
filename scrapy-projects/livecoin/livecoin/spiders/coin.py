f78159324236d3c5eefeea330c32f28990f1b39e
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-15 13:03:02 +0600

    Scrapped livebitcoin

--- scrapy-projects/livecoin/livecoin/spiders/coin.py
+++ scrapy-projects/livecoin/livecoin/spiders/coin.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class CoinSpider(scrapy.Spider):
     name = 'coin'
     allowed_domains = ['www.livecoin.net/en']
-    start_urls = ['http://www.livecoin.net/en/']
+
+    def start_requests(self):
+        yield SplashRequest(url='https://www.livecoin.net/en', callback=self.parse, endpoint="execute", args={
+            'lua_source': self.script
+        })
+
+    script = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+            ltc_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
+            ltc_tab[2]:mouse_click()
+            assert(splash:wait(1))
+            splash:set_viewport_full()
+            return splash:html()
+        end
+    '''
 
     def parse(self, response):
-        pass
+        for currency in response.xpath("//div[contains(@class, 'ReactVirtualized__Table__row tableRow___3EtiS ')]"):
+            yield {
+                'currency_pair': currency.xpath(".//div[1]/div/text()").get(),
+                'volume(24)': currency.xpath(".//div[2]/span/text()").get()
+            }
