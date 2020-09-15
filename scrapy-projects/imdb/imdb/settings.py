dbee756571816ae1d71ece1884a9eeba1ff1244d
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-15 20:38:18 +0600

    Store idmdb data to mongodb and sqlite

--- scrapy-projects/imdb/imdb/settings.py
+++ scrapy-projects/imdb/imdb/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'imdb.pipelines.ImdbPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'imdb.pipelines.ImdbPipeline': 300,  # this number determines the priority
+}
+
+MONGO_URI = "Hello World"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
